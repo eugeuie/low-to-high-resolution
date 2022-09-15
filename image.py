a6d9a818cf5d6f557394bf926da57439016e1382
Evgenia Abdullaeva <evabdullaeva@gmail.com>
2022-09-15 16:29:37 +0300

    kmeans image

--- image.py
+++ image.py
@@ -15,15 +15,15 @@ def read_image(img_path: str) -> np.array:
 def create_image(sample_img_path: str, img_path: str, data: np.array) -> None:
     sample_img = gdal.Open(sample_img_path, gdal.GA_ReadOnly)
     driver = sample_img.GetDriver()
-    x_size, y_size = img.RasterXSize, img.RasterYSize
+    x_size, y_size = sample_img.RasterXSize, sample_img.RasterYSize
     img = driver.Create(img_path, x_size, y_size, 1, gdal.GDT_UInt16)
     img.SetGeoTransform(sample_img.GetGeoTransform())
     img.SetProjection(sample_img.GetProjection())
     img.GetRasterBand(1).Fill(0)
     band = img.GetRasterBand(1)
     raster = np.zeros((y_size, x_size), dtype=np.uint8)
-    xs, ys = list(range(x_size)), list(range(y_size))
-    for y, x, value in zip(ys, xs, data):
-        raster[y][x] = value
+    for y in range(y_size):
+        for x in range(x_size):
+            raster[y][x] = data[x_size * y + x]
     band.WriteArray(raster)
     img = band = None
