235cb58421e7ae7c5116dd791299bcf306f60aee
Evgenia <evabdullaeva@gmail.com>
2022-10-03 12:10:49 +0000

    config and image modules

--- src/image.py
+++ src/image.py
@@ -1,5 +1,5 @@
 import numpy as np
-from osgeo import gdal
+from osgeo import gdal, osr
 
 
 def read_image(img_path: str) -> np.array:
@@ -27,3 +27,33 @@ def create_image(sample_img_path: str, img_path: str, data: np.array) -> None:
             raster[y][x] = data[x_size * y + x]
     band.WriteArray(raster)
     img = band = None
+
+
+def get_projection(img_path: str) -> str:
+    img = gdal.Open(img_path, gdal.GA_ReadOnly)
+    projection = img.GetProjection()
+    srs = osr.SpatialReference(wkt=projection)
+    projection = (
+        f"{srs.GetAttrValue('AUTHORITY', 0)}:{srs.GetAttrValue('AUTHORITY', 1)}"
+    )
+    return projection
+
+
+def reproject(img_path: str, reprojected_img_path: str, projection: str):
+    gdal.Warp(reprojected_img_path, img_path, dstSRS=projection)
+
+
+def read_rectangle(
+    img_path: str,
+    top_left_x: int,
+    top_left_y: int,
+    bottom_right_x: int,
+    bottom_right_y: int,
+) -> np.array:
+    img = gdal.Open(img_path, gdal.GA_ReadOnly)
+    x_size, y_size = img.RasterXSize, img.RasterYSize
+    band = img.GetRasterBand(1)
+    band_data = band.ReadAsArray(0, 0, x_size, y_size)
+    band_data = band_data.ravel()
+    img = band = None
+    return band_data
